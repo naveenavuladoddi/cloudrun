221906e1124fe070bd4801cb8a8a5479254eca62
naveen <naveen@gmail.com>
2025-03-13 14:33:46 +0530

    sample

--- amazingapp/buildall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable run.googleapis.com
-gcloud builds submit --tag gcr.io/${DEVSHELL_PROJECT_ID}/amazingapp:blue blue/
-gcloud builds submit --tag gcr.io/${DEVSHELL_PROJECT_ID}/amazingapp:green green/
-gcloud builds submit --tag gcr.io/${DEVSHELL_PROJECT_ID}/amazingapp:purple purple/
